dc6a3b7f1369b119100702fd94b623ddb878868f
Howard.Zuo <leftstick@qq.com>
2015-06-11 13:06:11 +0800

    update demo

--- etc/config.js
+++ etc/config.js
@@ -2,20 +2,20 @@
  *
  *  config.js which contains the configuration of app, and it should never be cached
  *
- *  @author  hao.zuo
- *  @date    Jun 9, 2015
+ *  @author  Howard.Zuo
+ *  @date    Jun 11, 2015
  *
  **/
 (function(global) {
     'use strict';
 
     global.__config = {
-        'appname': 'eee',
+        'appname': 'generator-es6-angular',
         'base': 'generator-es6-angular',
         'version': '1.0.0',
         'icp': 'ICP here',
         'protocol': 'https://raw.githubusercontent.com/leftstick/generator-require-angular/master/LICENSE',
-        'api': 'mock'
+        'api': '/mock'
     };
 
 }(window));
