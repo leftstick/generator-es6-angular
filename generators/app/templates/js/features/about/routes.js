3be407716699b93c8b20bfced6689900459cf41b
Howard.Zuo <leftstick@qq.com>
2017-01-05 15:47:16 +0800

    add about/routes.js back

---
+++ generators/app/templates/js/features/about/routes.js
@@ -0,0 +1,16 @@
+/**
+ *
+ *  Routes module expose route information used in about feature
+ *
+ *  @author  <%= answers.username %>
+ *  @date    <%= answers.date %>
+ *
+ */
+export default [
+    {
+        id: 'about',
+        isDefault: false,
+        when: '/about',
+        template: '<about></about>'
+    }
+];
