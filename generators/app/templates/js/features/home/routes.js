0d476708f30c38182dbdc87e9e54324792d65c25
Howard.Zuo <leftstick@qq.com>
2017-01-05 15:42:34 +0800

    add home/routes.js back

---
+++ generators/app/templates/js/features/home/routes.js
@@ -0,0 +1,16 @@
+/**
+ *
+ *  Routes module expose route information used in home feature
+ *
+ *  @author  <%= answers.username %>
+ *  @date    <%= answers.date %>
+ *
+ */
+export default [
+    {
+        id: 'home',
+        isDefault: true,
+        when: '/home',
+        template: '<home></home>'
+    }
+];
